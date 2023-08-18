729237fee8ae82efd1bd1a1b80ebc16033e6880a
Shishir435 <shishirchaurasiya435@gmail.com>
2023-08-19 01:56:04 +0530

    add favicon

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,11 +1,11 @@
+import Image from "next/image"
 import Link from "next/link"
-
-
-
+import imgUrl from '../android-chrome-192x192.png'
+const LectureSummaryUrl="https://chrome.google.com/webstore/detail/speedysynopsis/mnejhfjohfbgdoadgjmfigodfagokeme?gclid=Cj0KCQjwrfymBhCTARIsADXTabkhCVR6HhP-QqZ9VUOx6Q8VGSQeF6AnPrSByTXhznQOkJHtQwXT5oEaAjujEALw_wcB"
 const Navbar = () => {
   return (
     <nav className="flex justify-between p-10">
-        <div className="nav-right"> <Link href="/">Lecture Summary</Link></div>
+        <div className="nav-right"> <Link href={LectureSummaryUrl} target="_blank"><Image className="object-contain" src={imgUrl} width={32} height={32} alt="Lecture Summary"/></Link></div>
         <div className="nav-left flex gap-4">
             <Link href="https://shishirchaurasiya.site" target="_blank">Portfolio</Link>
             <Link href="https://linkedin.com/in/shishir-chaurasiya" target="_blank">Linkedin</Link>
